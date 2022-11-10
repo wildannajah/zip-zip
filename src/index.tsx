d435210af73d101395b8d806891c1cbd25a5dfea
Wildan Najah Wildiansyah <wildan.najah@gmail.com>
2022-11-10 22:29:30 +0700

    pwa

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider as ReduxProvider } from 'react-redux';
 import { PersistGate } from 'redux-persist/lib/integration/react';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 // redux
 import { store, persistor } from './redux/store';
@@ -26,6 +27,10 @@ root.render(
     </HelmetProvider>
   </React.StrictMode>,
 );
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/utils/uuidv4.ts
+++ src/utils/uuidv4.ts
@@ -2,6 +2,7 @@
 // ----------------------------------------------------------------------
 
 export default function uuidv4() {
+  // eslint-disable-next-line func-names
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
     const r = (Math.random() * 16) | 0;
     const v = c === 'x' ? r : (r & 0x3) | 0x8;
